743110a16d9af74646196043fc415a3ce38f20bc
DeenandData <hafsay.yussuf@gmail.com>
2025-09-14 12:46:40 +0300

    I have just learnt the basics of Javascript and specifically data types!

---
+++ Chapter 1/intro.js
@@ -0,0 +1,64 @@
+// alert('Welcome to Chapter 1!')
+//         let age=25;
+//         let year=2019;
+//         console.log(age, year);
+//         age=30;
+//         console.log(age);
+//         const points=100;
+//         console.log(points)
+//         // var score=75
+//         // console.log(score)
+//         // score=85
+//         // console.log(score);
+
+// Chapter 2
+// Strings
+// console.log('Hello, World!');
+
+// let email ="hafsay_yussuf@gmail.com";
+// console.log(email);
+
+// // String concatenation
+// let firstName= "Hafsa";
+// let lastName= "Yussuf";
+// let fullName= firstName + " " + lastName;
+// console.log(fullName);
+
+// // Getting characters
+// console.log(fullName[6]);
+
+// // String length
+// console.log(fullName.length);
+
+// // String methods
+// console.log(fullName.toUpperCase());
+// let result= fullName.toLowerCase();
+// console.log(result);
+// console.log(email);
+// console.log(email.indexOf('@'));
+
+// let index= email.indexOf('y');
+// console.log(index);
+
+// Practice Exercises
+let name= 'Hafsa';
+console.log(name, typeof name);
+
+let age= 25;
+console.log(age, typeof age);
+
+let isStudent= true;
+console.log(isStudent, typeof isStudent);
+
+let address= [324, "Nanyuki", "Kenya"];
+console.log(address, typeof address);
+
+let hobbies=["playing football", "reading", "coding", "running"];
+console.log(hobbies, typeof hobbies);
+
+let undefinedVariable;
+console.log(undefinedVariable, typeof undefinedVariable);
+
+let nullVariable= null;
+console.log(nullVariable, typeof nullVariable);
+
